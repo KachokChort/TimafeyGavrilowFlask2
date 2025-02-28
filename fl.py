a33e010eef11217293f25f9c64d46aea7fae8ab4
KachokChort <timajambam@gmail.com>
2025-02-28 17:54:59 +0300

    First

--- fl.py
+++ fl.py
@@ -28,10 +28,31 @@ def promotion():
             Присоединяйся!<p>"""
 
 
-@app.route('/image_sample')
-def image():
-    return f'''<img src="{url_for('static', filename='mars.jpg')}" 
-           alt="здесь должна была быть картинка, но не нашлась">'''
+@app.route('//image_mars')
+def bootstrap():
+    return f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='style.css')}" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="h">Ждт нас марс!</h1>
+
+    <img src="{url_for('static', filename='mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+    
+    <p style="font-size: 40px;">
+    <p class="p1">Человечество вырастает из детства.</p>
+    <p class="p2">Человечеству мала одна планета.</p>
+    <p class="p3">Мы сделаем обитаемыми безжизненные пока планеты.</p>
+    <p class="p4">И начнем с Марса!</p>
+    <p class="p5">Присоединяйся!</p><p>
+</body>
+</html>'''
 
 
 if __name__ == '__main__':
