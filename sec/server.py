4037f9976c9393969f7aa27044ec68208d099204
KachokChort <timajambam@gmail.com>
2025-03-11 18:00:46 +0300

    First

--- sec/server.py
+++ sec/server.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index(word):
     return render_template('base.html', title=word)
 
+@app.route('/training/<prog>')
+def training(prog):
+    return render_template('training.html', prog=prog)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
